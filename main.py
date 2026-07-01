b4eb58764b1efc96203c5b69e55d6c1678de9bbd
23f2000220 <23f2000220@ds.study.iitm.ac.in>
2026-07-01 21:36:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
-EMAIL = "your_email@example.com"
+EMAIL = "23f2000220@ds.study.iitm.ac.in"
 
 # ------------------------
 # Rate Limit Configuration
